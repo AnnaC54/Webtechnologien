15833797829d020c1676e67e42d32c324d2d076f
AnnaC54 <58769780+AnnaC54@users.noreply.github.com>
2021-11-17 15:37:47 +0100

    part I register validation (not finished)
    register validation

--- js/script.js
+++ js/script.js
@@ -1,72 +1,82 @@
-/*
-
-later: for token via angular (inf!) or php
-
-window.chatToken = "...";
-window.chatCollectionId = "...";
-window.chatServer = "https://online-lectures-cs.thi.de/chat";
 
 
+
+
+/* Teilaufgaben a: Registrieren
+
+Das Formular zum Registrieren soll die Eingabemöglichkeiten überprüfen und durch
+entsprechende Fehlermeldungen visualisieren, ob die Eingabe korrekt ist oder Fehler vorliegen.
+Solange Fehler vorliegen, soll das Formular nicht abgesendet werden können. Zu überprüfen
+sind die folgenden Aspekte:
+- DONE _Der gewählte Nutzername soll min. drei Zeichen lang sein
+-  Der gewählte Nutzername darf noch nicht verwendet worden sein, nutzen Sie hierfür
+- aus den Dummy-Datensatz und Informationen die Server-Funktion User Exists
+- Das Passwort muss min. 8 Zeichen haben
+- Die Passwort-Wiederholung muss dem Passwort entsprechen
+- Die visuelle Anzeige, ob die Eingabe korrekt ist oder Fehler vorliegen soll mittels CSS-KlassenWechsel am Eingabefeld umgesetzt werden (vgl. Experimente). Sie können hier zum Beispiel
+  die Farbe des Rahmens auf grün bzw. rot wechseln.
+
+  Hinweis: Recherchieren Sie nach Möglichkeiten, um mittels onsubmit-Ereignis das Absenden
+des Formulars zu verhindern.
 */
 
-/*Nutzen Sie dann zum Beispiel in den entsprechenden Aufrufen der XmlHttpRequest-Klasse die
-global verfügbaren Informationen:
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        let data = JSON.parse(xmlhttp.responseText);
-        console.log(data);
-    }
-};
+var chatServer = "https://online-lectures-cs.thi.de/chat";
+var chatToken; 
+var userTomToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MDcwMDI1fQ.lZ9CgGwXFKUrHdQIn8O0Zl2HorDtb9J0iTEaiZrYN60";
+var userJerryToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2MzcwNzAwMjV9.n5dIDlc_-3au_EmppGBSbXPoKHE2z2IZeMi2c-WvsRc";
+var collectionId = "185ead53-1b4c-40a3-beff-89c5560908a2";
+var testUserTrue = "Tom"; 
+var testUserFalse = "Yannick"; 
+
+
+/*
 // Chat Server URL und Collection ID als Teil der URL
 xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId +
     "/user", true);
-// Das Token zur Authentifizierung, wenn notwendig
-xmlhttp.setRequestHeader('Authorization', 'Bearer ' + window.chatToken);
-xmlhttp.send();*/
+*/
 
+var serverRequest = "https://online-lectures-cs.thi.de/chat/" + collectionId + "/user/" + testUserTrue;
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4) {
-        if(xmlhttp.status == 204) {
-            console.log("Exists");
-        } else if(xmlhttp.status == 404) {
-            console.log("Does not exist");
-        }
-    }
-};
-xmlhttp.open("GET", "https://online-lectures-cs.thi.de/chat/f7286ba2-ed3e-4266-a184-e8c21c9f3b03/user/Tom", true);
-xmlhttp.send();
+// password check;
 
+function password() {
 
-// password check 
-let username = document.getElementById("username");
-let password1;
-let password2;
-
-function pass()
-
-password1 = document.getElementById("password").value;
-password2 = document.getElementById("password-rep").value;
+    password1 = document.getElementById("password");
+    password2 = document.getElementById("password-rep");
 
     // If password not entered
     if (password1 == '')
-        alert ("Please enter Password");
-          
+        alert("Please enter Password");
+
     // If confirm password not entered
     else if (password2 == '')
-        alert ("Please enter confirm password");
-          
+        alert("Please enter confirm password");
+
     // If Not same return False.    
     else if (password1 != password2) {
-        alert ("\nPassword did not match: Please try again...")
+        alert("\nPassword did not match: Please try again...")
         return false;
     }
 
     // If same return True.
-    else{
+    else {
         alert("Password Match: Welcome to GeeksforGeeks!")
         return true;
+    }
 }
+
+// Server Request if user exists; 
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 204) {
+            console.log("Exists");
+        } else if (xmlhttp.status == 404) {
+            console.log("Does not exist");
+        }
+    }
+};
+xmlhttp.open("GET", serverRequest, true);
+xmlhttp.send();
