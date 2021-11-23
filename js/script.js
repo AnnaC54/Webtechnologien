33d4f7b96a89999f5d8c74ec839971f3999baea8
AnnaC54 <58769780+AnnaC54@users.noreply.github.com>
2021-11-23 09:31:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@ let conf = {}
 
 
 
-//var chatServer = "https://online-lectures-cs.thi.de/chat";
-//var userTomToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MDcwMDI1fQ.lZ9CgGwXFKUrHdQIn8O0Zl2HorDtb9J0iTEaiZrYN60";
-//var userJerryToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2MzcwNzAwMjV9.n5dIDlc_-3au_EmppGBSbXPoKHE2z2IZeMi2c-WvsRc";
-////var collectionId = "185ead53-1b4c-40a3-beff-89c5560908a2";
+var chatServer = "https://online-lectures-cs.thi.de/chat";
+var userTomToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MDcwMDI1fQ.lZ9CgGwXFKUrHdQIn8O0Zl2HorDtb9J0iTEaiZrYN60";
+var userJerryToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2MzcwNzAwMjV9.n5dIDlc_-3au_EmppGBSbXPoKHE2z2IZeMi2c-WvsRc";
+var collectionId = "185ead53-1b4c-40a3-beff-89c5560908a2";
 
 //var user = document.forms["myForm"]["fname"].value;
 //let password1 = document.forms["myForm"]["password"].value;
 //let password2 = document.forms["myForm"]["passwordConfirmation"].value;
-//var serverRequest = "https://online-lectures-cs.thi.de/chat/" + collectionId + "/user/" + userName;
+var serverRequest = "https://online-lectures-cs.thi.de/chat/" + collectionId + "/user/" + userName;
 //var serverRequest = `${conf.chatServer}${conf.collectionId}/user/+ userName` ;
 
 var user = document.getElementById("username");
