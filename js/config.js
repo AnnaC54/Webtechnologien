9e853953b3782f822ae26170493e0e0c6e191f8b
AnnaC54 <58769780+AnnaC54@users.noreply.github.com>
2021-11-23 09:24:20 +0100

    store out

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export function val(){
+    return {
+       "userTomToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjM3MDcwMDI1fQ.lZ9CgGwXFKUrHdQIn8O0Zl2HorDtb9J0iTEaiZrYN60",
+        "userJerryToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2MzcwNzAwMjV9.n5dIDlc_-3au_EmppGBSbXPoKHE2z2IZeMi2c-WvsRc",
+        "collectionId": "185ead53-1b4c-40a3-beff-89c5560908a2",
+        "chatServer": "https://online-lectures-cs.thi.de/chat"
+    }
+}
